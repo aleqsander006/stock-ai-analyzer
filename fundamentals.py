603448c8e6d4440208309047b2cd62c3097f147a
aleqsander006 <aleqsander006@gmail.com>
2026-07-16 22:14:31 +0400

    Implement get_fundamentals function for stock data
    Added a function to retrieve stock fundamentals using yfinance.

--- fundamentals.py
+++ fundamentals.py
@@ -1,1 +1,18 @@
+import yfinance as yf
 
+def get_fundamentals(ticker):
+
+    stock = yf.Ticker(ticker)
+    info = stock.info
+
+    return {
+        "Company": info.get("longName", "N/A"),
+        "Sector": info.get("sector", "N/A"),
+        "Industry": info.get("industry", "N/A"),
+        "Country": info.get("country", "N/A"),
+        "Market Cap": info.get("marketCap", "N/A"),
+        "P/E Ratio": info.get("trailingPE", "N/A"),
+        "EPS": info.get("trailingEps", "N/A"),
+        "Dividend Yield": info.get("dividendYield", "N/A"),
+        "Employees": info.get("fullTimeEmployees", "N/A")
+    }
