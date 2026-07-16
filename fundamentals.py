652f7deffe194e7df26d1e6f0b151a7577ff3402
aleqsander006 <aleqsander006@gmail.com>
2026-07-16 22:49:57 +0400

    Modify get_fundamentals function defaults
    Updated default values for Sector and Industry to 'N/A'. Removed caching decorator.

--- fundamentals.py
+++ fundamentals.py
@@ -1,13 +1,9 @@
-import streamlit as st
-
-
-@st.cache_data(ttl=3600)
 def get_fundamentals(ticker):
 
     return {
         "Company": ticker,
-        "Sector": "Loading later",
-        "Industry": "Loading later",
+        "Sector": "N/A",
+        "Industry": "N/A",
         "Country": "N/A",
         "Market Cap": "N/A",
         "P/E Ratio": "N/A",
